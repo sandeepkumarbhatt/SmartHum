0da0213123b4cf5f2656f33d600e7b0fea17a388
sandeep.bhatta <sandeep.bhatta.intl.verizon.com>
2017-09-15 17:29:26 +0530

    Intitial commit

---
+++ app.js
@@ -0,0 +1,22 @@
+var restify = require('restify');
+var builder = require('botbuilder');
+
+// Setup Restify Server
+var server = restify.createServer();
+server.listen(process.env.port || process.env.PORT || 3978, function () {
+   console.log('%s listening to %s', server.name, server.url); 
+});
+
+// Create chat connector for communicating with the Bot Framework Service
+var connector = new builder.ChatConnector({
+    appId: '8459c78c-d98e-4073-a369-64368af151de',
+    appPassword: 'p43JVgFdkBWSxa8S6Cq2r4K'
+});
+
+// Listen for messages from users 
+server.post('/api/messages', connector.listen());
+
+// Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
+var bot = new builder.UniversalBot(connector, function (session) {
+    session.send("You said111: %s", session.message.text);
+});
---
+++ readme.txt
@@ -0,0 +1,3 @@
+App Password: p43JVgFdkBWSxa8S6Cq2r4K
+
+AppId - 8459c78c-d98e-4073-a369-64368af151de
